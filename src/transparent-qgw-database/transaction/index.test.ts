f45424a868c4143bd545669d2d45b8a44abd075c
pjfitacc <pjfitacc@gmail.com>
2025-04-21 09:25:03 -0400

    Recurring Options Tests

--- src/transparent-qgw-database/transaction/index.test.ts
+++ src/transparent-qgw-database/transaction/index.test.ts
@@ -26,33 +26,8 @@ describe("TransactionRequest: Default TransQGW Options field values", () => {
     expect(payload.Dsep).toBe(undefined);
     expect(payload.MAXMIND).toBe(undefined);
   });
-});
 
-describe("TransactionRequest: Default TransQGW Options field values", () => {
-  it("If TransactionRequest has recurringOptions, there MUST be a RID", () => {
-    const payer = new Payer("123 street", "10001", "email@email.com");
-    const eft = new ElectronicFundsTransfer("123123123", "123123123123");
-    const payment = new Payment(200, eft);
-    const options = new Options();
-
-    const transactionRequest = new TransactionRequest(
-      payment,
-      payer,
-      options,
-      new RecurringOptions({ rid: "1" })
-    );
-    const payload = transactionRequest.toAPI();
-
-    expect(payload.RID).toBe("1");
-    expect(payload.override_recur).toBe(undefined);
-    expect(payload.override_trans_email).toBe(undefined);
-    expect(payload.recur_times).toBe(undefined);
-    expect(payload.initial_amount).toBe(undefined);
-  });
-});
-
-describe("TransactionRequest: Default TransQGW Options field values", () => {
-  it("If Options, has emailCustomerReceipt and sendTransactionEmail true, the DirectAPI equivalent should be Y, false should be N", () => {
+  it("If Options has emailCustomerReceipt and sendTransactionEmail true, the DirectAPI equivalent should be Y, false should be N", () => {
     const payer = new Payer("123 street", "10001", "email@email.com");
     const eft = new ElectronicFundsTransfer("123123123", "123123123123");
     const payment = new Payment(200, eft);
@@ -81,10 +56,8 @@ describe("TransactionRequest: Default TransQGW Options field values", () => {
     expect(newPayload.override_email_customer).toBe("N");
     expect(newPayload.override_trans_email).toBe("N");
   });
-});
 
-describe("TransactionRequest: Default TransQGW Options field values", () => {
-  it("If Options, has maxMindOn true, the DirectAPI equivalent should be 1, false should be 2", () => {
+  it("If Options has maxMindOn true, the DirectAPI equivalent should be 1, false should be 2", () => {
     const payer = new Payer("123 street", "10001", "email@email.com");
     const eft = new ElectronicFundsTransfer("123123123", "123123123123");
     const payment = new Payment(200, eft);
@@ -105,5 +78,64 @@ describe("TransactionRequest: Default TransQGW Options field values", () => {
     const newPayload = transactionRequest.toAPI();
 
     expect(newPayload.MAXMIND).toBe("2");
+  });
+});
+
+describe("TransactionRequest: Default TransQGW Recurring Options field values", () => {
+  it("If TransactionRequest has recurringOptions, there MUST be a RID", () => {
+    const payer = new Payer("123 street", "10001", "email@email.com");
+    const eft = new ElectronicFundsTransfer("123123123", "123123123123");
+    const payment = new Payment(200, eft);
+    const options = new Options();
+
+    const transactionRequest = new TransactionRequest(
+      payment,
+      payer,
+      options,
+      new RecurringOptions({ rid: "1" })
+    );
+    const payload = transactionRequest.toAPI();
+
+    expect(payload.RID).toBe("1");
+    expect(payload.override_recur).toBe(undefined);
+    expect(payload.OverRideRecureDay).toBe(undefined);
+    expect(payload.recur_times).toBe(undefined);
+    expect(payload.initial_amount).toBe(undefined);
+  });
+
+  it("If RecurringOptions has overrideRecurringPrice and overrideRecurringDay true, the DirectAPI equivalent should be Y, false should be N", () => {
+    const payer = new Payer("123 street", "10001", "email@email.com");
+    const eft = new ElectronicFundsTransfer("123123123", "123123123123");
+    const payment = new Payment(200, eft);
+    const recurringOptions = new RecurringOptions({
+      rid: "1",
+      overrideRecurringPrice: true,
+      overrideRecurringDay: true,
+    });
+
+    const transactionRequest = new TransactionRequest(
+      payment,
+      payer,
+      undefined,
+      recurringOptions
+    );
+    const payload = transactionRequest.toAPI();
+
+    expect(payload.RID).toBe("1");
+    expect(payload.override_recur).toBe("Y");
+    expect(payload.OverRideRecureDay).toBe("Y");
+    expect(payload.recur_times).toBe(undefined);
+    expect(payload.initial_amount).toBe(undefined);
+
+    transactionRequest.recurringOptions = new RecurringOptions({
+      rid: "1",
+      overrideRecurringPrice: false,
+      overrideRecurringDay: false,
+    });
+
+    const newPayload = transactionRequest.toAPI();
+
+    expect(newPayload.override_recur).toBe("N");
+    expect(newPayload.OverRideRecureDay).toBe("N");
   });
 });
