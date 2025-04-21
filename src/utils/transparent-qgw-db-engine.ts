a85509e1b31c9d659c419ea6c43509e5f2b04880
pjfitacc <pjfitacc@gmail.com>
2025-04-21 09:05:34 -0400

    Toggle1Or2

--- src/utils/transparent-qgw-db-engine.ts
+++ src/utils/transparent-qgw-db-engine.ts
@@ -3,7 +3,7 @@ export function toggleYesOrNO(boolean?: boolean): "Y" | "N" | undefined {
   return boolean ? "Y" : "N";
 }
 
-export function toggleBinary(boolean?: boolean): "1" | "2" | undefined {
+export function toggle1or2(boolean?: boolean): "1" | "2" | undefined {
   if (boolean === undefined) return undefined;
   return boolean ? "1" : "2";
 }
