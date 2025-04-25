326d7e2ee3f492c95fd116e95ae079a2f7e18d70
pjfitacc <pjfitacc@gmail.com>
2025-04-25 12:10:57 -0400

    Custom Error Class + Custom Issue for the Library

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -1,21 +1,43 @@
+/*
+CustomIssue Interface (copies: https://zod.dev/ERROR_HANDLING?id=zodissue):
+
+code
+
+path?
+
+message
+*/
+interface CustomIssue {
+  code: any;
+  path?: (string | number)[];
+  message: string;
+}
+
+/*
+message - a human readable display for what error our library caught.
+
+error code - the custom error. can be ERR_PARSE which is a library error or ERR_SERVER_RESPONSE which is an error from outside the library coming from Transparent Quantum Gateway.
+
+Issues = an Array of class CustomIssue related to the overall message + error code.
+*/
 class CustomError<C extends string> extends Error {
   message: string;
-  statusCode: number;
+  issues: CustomIssue[];
   code?: C;
 
   constructor({
     message,
-    statusCode,
+    issues,
     code,
   }: {
     message: string;
-    statusCode: number;
+    issues: CustomIssue[];
     code?: C;
   }) {
     super();
     this.message = message;
-    this.statusCode = statusCode;
     this.code = code;
+    this.issues = issues;
   }
 }
 
